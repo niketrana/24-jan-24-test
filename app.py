cdd9b0aaaa4b0ab47a4067f421ba0192d24204f3
niketrana <72081350+niketrana@users.noreply.github.com>
2024-01-24 09:33:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 # app.py
-print("Hello, OpenShift! This is a Python application.")
+#print("Hello, OpenShift! This is a Python application.")
+
+# app.py
+while True:
+    print("Hello, OpenShift! This is a Python application.")
+    # Add any other processing logic as needed
